3c59f2850be47654ab65a5c1f06c50b8c58901db
Alfonsina <alfonsina.l@gmail.com>
2019-07-06 23:19:56 -0400

    Modified h-section-svg class to id, added grid-container class to grid partial to include all sections, added header styles and media queries, added some padding to navigation, added script folder to modify header svg viewbox

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+// Function to change header svg viewbox
+var svg = document.getElementById('h-section-svg'),
+    originalViewBox = svg.getAttribute('viewBox'), /* Store the original value in a variable */
+    mq = matchMedia("(max-width: 850px)"), /* Define our media query and media query object */
+    updateViewBox;
+
+/* Define the handler */
+updateViewBox = function(){
+    if (mq.matches) {
+        /* Change the viewBox dimensions */
+        svg.setAttribute('viewBox', "500 0 950 865");
+    } else {
+        svg.setAttribute('viewBox', originalViewBox);
+    }
+}
+
+/* Fire on document load */
+// WebKit/Blink browsers
+svg.onload = updateViewBox;
+
+// Firefox & IE
+//console.log(svg.addEventListener('SVGLoad', updateViewBox, true));
+
+/* Fire if the media condition changes */
+mq.addListener(updateViewBox);            
+
